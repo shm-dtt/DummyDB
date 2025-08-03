fabb3ce491ef98e4aaa7e864a9b34eb48f2cbb80
shm-dtt <77527904+shm-dsgn@users.noreply.github.com>
2025-08-03 22:48:00 +0530

    Revert

--- ui/next.config.ts
+++ ui/next.config.ts
@@ -1,21 +1,8 @@
 import type { NextConfig } from "next";
-import nextra from "nextra";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-const withNextra = nextra({});
-
-export default withNextra({
-  // contentDirBasePath: '/app/docs',
-  turbopack: {
-    resolveAlias: {
-      // Path to your `mdx-components` file with extension
-      "next-mdx-import-source-file": "./mdx-components.tsx",
-    },
-  },
-  nextConfig
-});
-
+export default nextConfig;
 
