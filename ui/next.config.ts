488e0973225181e52b3e43b3bc22abd52b88e7e7
shm-dtt <77527904+shm-dsgn@users.noreply.github.com>
2025-08-03 22:29:19 +0530

    Added nextra. Currently Testing

--- ui/next.config.ts
+++ ui/next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
+import nextra from "nextra";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+const withNextra = nextra({});
+
+export default withNextra({
+  // contentDirBasePath: '/app/docs',
+  turbopack: {
+    resolveAlias: {
+      // Path to your `mdx-components` file with extension
+      "next-mdx-import-source-file": "./mdx-components.tsx",
+    },
+  },
+  nextConfig
+});
+
+
