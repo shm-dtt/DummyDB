15b77d6af7e5903ebe907ea998b94d1811a91834
shm-dtt <77527904+shm-dsgn@users.noreply.github.com>
2025-08-06 00:54:40 +0530

    font issue fixed

--- ui/app/layout.tsx
+++ ui/app/layout.tsx
@@ -54,7 +54,7 @@ export default async function RootLayout({
       >
         {/* Your additional tags should be passed as `children` of `<Head>` element */}
       </Head>
-      <body className="antialiased !font-sans">
+      <body className={`${geistSans.variable} ${geistMono.variable} font-geist-sans antialiased`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
